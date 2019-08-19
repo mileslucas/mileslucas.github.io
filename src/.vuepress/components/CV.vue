04c474aa784048fe9e12ad564c5195ebb1a97e9b
Miles Lucas <mdlucas@iastate.edu>
2019-08-19 00:22:11 -1000

    update cv to dynamically choose when to render

--- src/.vuepress/components/CV.vue
+++ src/.vuepress/components/CV.vue
@@ -1,17 +1,39 @@
 <template>
-    <object data="/CV.pdf" 
-        type="application/pdf" 
-        width="80%"
-        height="600rem"
-        style="
-        margin-right: auto;
-        margin-left: auto;
-        display: block;
-        ">
-
-        <a href="/CV.pdf">Download link</a>
-        <embed src="/CV.pdf">
-            <p>This browser does not support PDFs. Please download the PDF to view it: <a href="/CV.pdf">Download PDF</a>.</p>
-        </embed>
+  <div v-if="render">
+    <object
+      id="pdf"
+      data="/CV.pdf"
+      type="application/pdf"
+      width="80%"
+      height="600rem"
+      style="
+        
+        "
+    >
+      <embed src="/CV.pdf" />
     </object>
+  </div>
+  <div v-else>
+    <a href="/CV.pdf">Download link</a>
+  </div>
 </template>
+
+<script>
+const pdfSupport = require("browser-pdf-support");
+
+export default {
+  data() {
+    return {
+      render: pdfSupport()
+    };
+  }
+};
+</script>
+
+<style scoped>
+#pdf {
+  margin-right: auto;
+  margin-left: auto;
+  display: block;
+}
+</style>
